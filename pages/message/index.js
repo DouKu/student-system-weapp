1364c91bf3a7a9963a815530a36baea8b3336092
lgybetter <437675103@qq.com>
2019-05-13 23:42:12 +0800

    :art: 信息修改

--- pages/message/index.js
+++ pages/message/index.js
@@ -1,4 +1,5 @@
 // pages/message/index.js
+const { $Message } = require('../../components/base/index');
 Page({
 
   /**
@@ -13,22 +14,18 @@ Page({
       id: 2,
       name: '女'
     }],
-    sex: '',
-    isDorm: '否',
-    first_subject: '',
-    second_subject: [],
     message: {
-      name: '',
-      sex: 0,
-      tel_num: '',
-      address: '',
-      account_location: '',
-      is_dorm: 0,
-      guardian_name: '',
-      guardian_tel_num: '',
-      guardian_id_card: '',
-      first_subject: 0,
-      second_subject: []
+      name: '林光裕',
+      sex: '男',
+      tel_num: '15521132074',
+      address: 'test',
+      account_location: 'test',
+      is_dorm: '否',
+      guardian_name: 'test',
+      guardian_tel_num: 'test',
+      guardian_id_card: 'test',
+      first_subject: '物理',
+      second_subject: ['化学', '生物']
     }
   },
 
@@ -93,30 +90,187 @@ Page({
     });
   },
   toSecond: function () {
+    const message = this.data.message;
+    if (!message.sex) {
+      return $Message({
+        content: '请选择性别',
+        type: 'error'
+      })
+    }
+    if (!message.tel_num) {
+      return $Message({
+        content: '请填写电话号码',
+        type: 'error'
+      })
+    }
+    if (!message.address) {
+      return $Message({
+        content: '请填写家庭住址',
+        type: 'error'
+      })
+    }
+    if (!message.account_location) {
+      return $Message({
+        content: '请填写户口所在地',
+        type: 'error'
+      })
+    }
+    if (!message.is_dorm) {
+      return $Message({
+        content: '请选择是否内宿',
+        type: 'error'
+      })
+    }
     this.setData({
       step: 1
     });
   },
   toThird: function () {
+    const message = this.data.message;
+    if (!message.guardian_name) {
+      return $Message({
+        content: '请填写监护人姓名',
+        type: 'error'
+      })
+    }
+    if (!message.guardian_tel_num) {
+      return $Message({
+        content: '请填写监护人电话号码',
+        type: 'error'
+      })
+    }
+    if (!message.guardian_id_card) {
+      return $Message({
+        content: '请填写监护人身份证号',
+        type: 'error'
+      })
+    }
     this.setData({
       step: 2
     });
   },
   handleSexChange: function (e) {
     this.setData({
-      sex: e.detail.value
+      message: {
+        ...this.data.message,
+        sex: e.detail.value
+      }
     });
   },
   handleIsDormChange: function (e) {
     this.setData({
-      isDorm: e.detail.value
+      message: {
+        ...this.data.message,
+        isDorm: e.detail.value
+      }
     });
   },
   handleFirstSubjectChange: function (e) {
     this.setData({
-      first_subject: e.detail.value
+      message: {
+        ...this.data.message,
+        first_subject: e.detail.value
+      }
     });
   },
   handleSecondSubjectChange: function (e) {
+    const subject = e.detail.value;
+    let second_subject = this.data.message.second_subject;
+    if (second_subject.length < 2) {
+      second_subject.push(subject);
+    } else {
+      second_subject.shift()
+      second_subject.push(subject);
+    }
+    this.setData({
+      message: {
+        ...this.data.message,
+        second_subject
+      }
+    });
   },
+  handleSubmit: function () {
+    const message = this.data.message;
+    if (!message.first_subject) {
+      return $Message({
+        content: '请进行科目二选一',
+        type: 'error'
+      })
+    }
+    if (message.second_subject.length !== 2) {
+      return $Message({
+        content: '请进行科目四选二',
+        type: 'error'
+      })
+    }
+    const token = wx.getStorageSync('token');
+    wx.request({
+      url: 'http://127.0.0.1:7001/api/user/auth/user',
+      method: 'PUT',
+      header: {
+        Authorization: `Bearer ${token}`
+      },
+      data: this.data.message,
+      success: (res) => {
+        $Message({
+          content: '信息修改成功',
+          type: 'success'
+        })
+      },
+      fail: () => {
+        $Message({
+          content: '信息修改失败',
+          type: 'error'
+        })
+      }
+    });
+  },
+  handleNameChange: function (e) {
+    this.setData({
+      message: {
+        ...this.data.message,
+        name: e.detail.detail.value
+      }
+    })
+  },
+  handleTemNumChange: function (e) {
+    this.setData({
+      message: {
+        ...this.data.message,
+        tel_num: e.detail.detail.value
+      }
+    })
+  },
+  handleAddressChange: function (e) {
+    this.setData({
+      message: {
+        ...this.data.message,
+        address: e.detail.detail.value
+      }
+    })
+  },
+  handleAccountLoactionChange: function (e) {
+    this.setData({
+      message: {
+        ...this.data.message,
+        account_location: e.detail.detail.value
+      }
+    })
+  },
+  handleGuardianNameChange: function (e) {
+    this.setData({
+      message: {
+        ...this.data.message,
+        guardian_name: e.detail.detail.value
+      }
+    })
+  },
+  handleGuardianTelNumChange: function (e) {
+    this.setData({
+      message: {
+        ...this.data.message,
+        guardian_id_card: e.detail.detail.value
+      }
+    })
+  }
 });
